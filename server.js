ba16e2508d23f353bf97a72542227fd8f98c5bd0
AkifhanIlgaz <akifhanilgaz@gmail.com>
2022-10-10 15:21:12 +0300

    Update

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const messages = [
+  { name: "Tim", message: "Hi" },
+  { name: "Jane", message: "Hello" },
+];
+
+app.get("/messages", (req, res) => {
+  res.send(messages);
+});
+
+app.post("/messages", (req, res) => {
+  messages.push(req.body);
+  io.emit("message", req.body);
+  res.sendStatus(200);
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+});
+
+const server = http.listen(4000, () => {
+  console.log(`Server is listening on port ${server.address().port}`);
+});
