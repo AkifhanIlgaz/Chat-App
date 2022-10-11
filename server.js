1a6f4a5f1942fd545339e5669aad9600d3c77e4a
AkifhanIlgaz <akifhanilgaz@gmail.com>
2022-10-11 09:40:04 +0300

    Update

--- server.js
+++ server.js
@@ -3,28 +3,42 @@ const bodyParser = require("body-parser");
 const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
+const mongoose = require("mongoose");
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const messages = [
-  { name: "Tim", message: "Hi" },
-  { name: "Jane", message: "Hello" },
-];
+const dbURL = "mongodb+srv://user:user@node-js.mwko205.mongodb.net/test";
+
+const Message = mongoose.model("Message", {
+  name: String,
+  message: String,
+});
 
 app.get("/messages", (req, res) => {
-  res.send(messages);
+  Message.find({}, (err, messages) => {
+    res.send(messages);
+  });
 });
 
 app.post("/messages", (req, res) => {
-  messages.push(req.body);
-  io.emit("message", req.body);
-  res.sendStatus(200);
+  const message = new Message(req.body);
+
+  message.save((err) => {
+    if (err) res.send(500);
+
+    io.emit("message", req.body);
+    res.sendStatus(200);
+  });
 });
 
 io.on("connection", (socket) => {
   console.log("User connected");
+});
+
+mongoose.connect(dbURL, (err) => {
+  console.log("MongoDB connection", err);
 });
 
 const server = http.listen(4000, () => {
